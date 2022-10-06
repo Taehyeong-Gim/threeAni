d41092d2c7274949028acb47b88f9992bb62e538
Taehyeong Kim <kth@pspace.ai>
2022-10-06 15:01:51 +0900

    impl : present animation

--- src/index.js
+++ src/index.js
@@ -11,13 +11,15 @@ const component = () =>{
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
-  // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  // const cube = new THREE.Mesh( geometry, material );
-  // scene.add( cube );
+
   new GLTFLoader().load('models/Present.glb', function ( gltf ) {
     scene.add( gltf.scene );
-    animate(gltf)
+    const mixer = new THREE.AnimationMixer(gltf.scene);
+    const clips = gltf.animations;
+    const basicAction = mixer.clipAction(clips[0]);
+    console.log(basicAction);
+    basicAction.play();
+    animate(gltf, mixer)
   }, undefined, function ( error ) {
     console.error( error );
   } );
@@ -29,11 +31,9 @@ const component = () =>{
   const lighta2 = new THREE.PointLight( 0xFFFFFF, 1, 100 );
   lighta2.position.set( -10, 3, -10 );
   scene.add( lighta2 ); 
-  function animate(object) {
-    requestAnimationFrame( animate.bind(null,object) );
-    
-    object.scene.rotation.x += 0.01;
-    object.scene.rotation.y += 0.01;
+  function animate(object, mixer) {
+    requestAnimationFrame( animate.bind(null,object, mixer) );
+    mixer.update(1/60);
     renderer.render( scene, camera );
   };
 
